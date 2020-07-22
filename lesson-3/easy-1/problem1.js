d369d52bbcced564b546161b0b6156f857f7fdf9
kylemuch <kylemuch@gmail.com>
2020-07-22 15:20:04 -0700

    p1 done

---
+++ lesson-3/easy-1/problem1.js
@@ -0,0 +1,17 @@
+//will the code below raise an error?
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+
+//no, you can insert an element anywhere within an array. The empty spaces will show as undefined.
+
+
+let numbers = [1, 2, 3];
+numbers[6] = 5;
+numbers[4];  // what will this line return?
+
+//undefined
+
+
+
+
--- lesson-3/easy-1/q3.js
+++
@@ -1,9 +0,0 @@
-//Determine whether the following object of people and their age contains an entry for 'Spot':
-
-let ages = { Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10 };
-
-//My Solution
-Object.keys(ages).includes('Spot');
-
-//Solution
-ages.hasOwnProperty("Spot");
